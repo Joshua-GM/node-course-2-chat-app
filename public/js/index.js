6f15de6991fa955919b9d5e2f80418fa457d25a0
Dynamic Duo <DynamicDuo@MacBook-Pro.local>
2018-06-13 12:05:53 +0800

    Add geolocation via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,14 @@
 const socket = io();
+let user;
+
+jQuery('#message-form, #user').hide();
+
 socket.on('connect', function() {
 	console.log('Connected to server');
+});
 
-// 	socket.emit('createEmail', {
-// 		to: "jen@example.com",
-// 		text: "Hey. this is Jen"
-// 	});
-
-// 	socket.emit('createMessage', {
-// 		from: "joshua1234",
-// 		text: "Hey, are u on PUBG now?"
-// 	})
-
+socket.on('disconnect', function(){
+	console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function(message){
@@ -22,17 +19,50 @@ socket.on('newMessage', function(message){
 	jQuery('#messages').append(li);
 });
 
-socket.on('disconnect', function(){
-	console.log('Disconnected from server');
+socket.on('newLocationMessage', function(message){
+	var li = jQuery('<li></li>');
+	var a = jQuery('<a target="_blank">My Current Location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+
+	jQuery('#messages').append(li);
+});
+
+jQuery("#username-form").on('submit', function(e){
+	e.preventDefault();
+	user = jQuery('[name=username').val();
+	let useradd=`User: ${user}`;
+	jQuery("#username-form").hide();
+	jQuery("#user").text(useradd);
+	jQuery('#message-form, #user').show();
+
 });
 
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault();
 
 	socket.emit('createMessage', {
-		from: 'User',
+		from: user,
 		text: jQuery('[name=message]').val()
 	}, function() {
 
+	});
+});
+
+var locationButton = jQuery("#send-location");
+locationButton.on('click', function(){
+	if (!navigator.geolocation) {
+		return alert('Not supported by your browser!');
+	}
+	navigator.geolocation.getCurrentPosition(function(position){
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+
+	}, function(){
+		alert('Unable to fetch location!');
 	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', ()=>{
 	it('should generate correct message object', ()=>{
@@ -9,5 +9,19 @@ describe('generateMessage', ()=>{
 
 		expect(typeof res.createdAt).toBe('number');
 		expect(res).toMatchObject({from, text});
+	});
+});
+
+describe('generateLocationMessage', ()=>{
+	it('should generate correct location object', ()=>{
+		const from = 'Admin';
+		const latitude = 12.232;
+		const longitude = 12.3434;
+		const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+		const res = generateLocationMessage(from, latitude, longitude);
+		expect(typeof res.createdAt).toBe('number');
+		expect(res).toMatchObject({from});
+		expect(res.url).toBe(url);
 	});
 });
