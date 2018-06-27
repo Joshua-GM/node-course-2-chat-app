be9118f6e068db255be820593007992ac512e764
Dynamic Duo <DynamicDuo@MacBook-Pro.local>
2018-06-27 10:58:20 +0800

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -13,25 +13,34 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
 	let formattedTime = moment(message.createdAt).format('h:mm a');
+	let template = jQuery('#message-template').html();
+	let html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	console.log('New Message', message);
-	var li = jQuery('<li></li>');
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
+	jQuery('#messages').append(html);
 
-	jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
 	let formattedTime = moment(message.createdAt).format('h:mm a');
+	let locationTemplate = jQuery("#location-message-template").html();
+	let html = Mustache.render(locationTemplate, {
+		from: message.from,
+		url: message.url,
+		createdAt: formattedTime
+	});
 
-	var li = jQuery('<li></li>');
-	var a = jQuery('<a target="_blank">My Current Location</a>');
+	// var li = jQuery('<li></li>');
+	// var a = jQuery('<a target="_blank">My Current Location</a>');
 
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url);
-	li.append(a);
+	// li.text(`${message.from} ${formattedTime}: `);
+	// a.attr('href', message.url);
+	// li.append(a);
 
-	jQuery('#messages').append(li);
+	jQuery('#messages').append(html);
 });
 
 jQuery("#username-form").on('submit', function(e){
